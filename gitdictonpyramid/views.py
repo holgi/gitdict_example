66a5a49de6f9eafc40a5a4cbb32406cd52880888
Holger Frey <mail@holgerfrey.de>
2016-01-23 02:20:11 +0100

    added a 404 view

--- gitdictonpyramid/views.py
+++ gitdictonpyramid/views.py
@@ -1,4 +1,4 @@
-from pyramid.view import view_config
+from pyramid.view import view_config, notfound_view_config
 from pyramid.response import Response
 from pyramid.httpexceptions import HTTPNotFound
 
@@ -24,25 +24,33 @@ def git_file_download(context, request):
 
 @view_config(context=gitdict.File, name='history', renderer='templates/history.pt')
 def git_file_history(context, request):
+    # history is only available for text files
     mtype, encoding = mimetypes.guess_type(context.__name__)
     if mtype is None or not mtype.startswith('text/'):
-        # history is only available for text files
         raise HTTPNotFound()
+    # collect all commits except the current one
     old_commits = list(context.history)[1:]
     if old_commits:
+        # get the diffs between the commits
         reference = context.last_commit
         changes = []
         for commit in old_commits:
             patch = context.diff(commit, reference=reference)
             changes.append((patch, reference))
-            reference = commit    
+            reference = commit
+        # get the first commit and version of the file
         first_file_commit = old_commits[-1]
         first_pygit_file = context._get_object_from_commit(first_file_commit)
         first_file_content = first_pygit_file.data.decode('utf-8')
     else:
+        # this file has no changes
         changes = []
         first_file_content = context.text
         first_file_commit = context.last_commit
     return {'changes': changes, 
             'first_file_content': first_file_content,
             'first_file_commit': first_file_commit}
+
+@notfound_view_config(renderer='templates/404.pt')
+def whoops(context, request):
+    return {}
